fe2894ae910fac212847e469566dd1f8dba0ba57
tam <jtam6@live.com>
2022-11-13 06:39:04 -0800

    third

--- src/L08.java
+++ src/L08.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 public class L08 {
     public static void main(String[] args) {
@@ -5,9 +6,12 @@ public class L08 {
         // Test your radix sort implementation here
         String[] arr = generateStringArray(3, 8);
         displayStringArray(arr);
-        radixSort(arr);
+
         System.out.println();
-        displayStringArray(arr);
+//        System.out.println(Arrays.toString(radixSort(arr)));
+//        radixSort(arr);
+     //   displayStringArray(arr);
+      displayStringArray(radixSort(arr));
 
 
     }
@@ -15,7 +19,7 @@ public class L08 {
     // TO DO:
     // Implement
     // NOTE: You can assume that all strings in the array have the same length
-    public static void radixSort(String[] arr) {
+    public static String[] radixSort(String[] arr) {
 
 
         //ascii码的取值范围
@@ -41,21 +45,27 @@ public class L08 {
                 }
                 int sum = 0;
                 for (int i = 0; i < cntArr.length; i++) {
-                    sum += cntArr[i];
-                    cntArr[i] = sum;
+
+                    sum += cntArr[i];cntArr[i] = sum;
+
+
+
                 }
 
-                for (int i = arr.length - 1; i >= 0; i--) {
+                for (int i = 0; i< arr.length ; i++) {
                     int index = getIndex(arr[i], k);
-                    sortArr[cntArr[index] - 1] = arr[i];
+
+
+                    sortArr[arr.length-cntArr[index] ] = arr[i];
                     cntArr[index]--;
                 }
 
                 arr = sortArr.clone();
+
             }
-         //   return arr;
+          return arr;
         }
-        //获取字符串第k位字符所对应的ascii码序号
+        //get char ascii num
         private static int getIndex (String str,int k){
             //put
             if (str.length() < k + 1) {
@@ -66,7 +76,7 @@ public class L08 {
 
 
         //////* HELPER METHODS *//////
-        private static String[] generateStringArray ( int wordLen, int arrLen){
+         static String[] generateStringArray ( int wordLen, int arrLen){
             String[] array = new String[arrLen];
 
             for (int i = 0; i < arrLen; i++) {
@@ -80,9 +90,9 @@ public class L08 {
             return array;
         }
 
-        private static void displayStringArray (String[]array){
+         static void displayStringArray (String[] array){
 
-for (String s : array) {
+        for (String s : array) {
                 System.out.println(s);
             }
         }
